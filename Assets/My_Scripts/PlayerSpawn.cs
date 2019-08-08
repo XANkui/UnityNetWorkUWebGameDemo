4df06e54f1a5999e85a1575bcc812289e0bd3962
xan <xan@pacificfuture.co>
2019-08-08 19:54:11 +0800

    Unity Unet 实现全网段联网（利用Unity的服务器，所以有网络延迟），通过房间名，建立房间，进入房间

---
+++ Assets/My_Scripts/PlayerSpawn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSpawn : NetworkBehaviour
+{
+
+    public GameObject My_plane;
+    public Transform[] NetworkStartPosition;
+    int spawnCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void OnStartClient()
+    {
+
+        Debug.Log("PlayerSpawn / OnStartClient");
+        spawnCount = NetworkStartPosition.Length;
+        for (int i =0; i < spawnCount; i++)
+        {
+            if (NetworkStartPosition[i].childCount == 0) {
+                GameObject player = Instantiate(My_plane, NetworkStartPosition[i]) as GameObject;
+                //NetworkServer.Spawn(player);
+                break;
+                
+            }
+        }            
+    }
+
+}
